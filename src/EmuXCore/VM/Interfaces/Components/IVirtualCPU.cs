cbe55802ac65dfb7db2389103823db81ccdb0cc4
RysteQ <34135554+RysteQ@users.noreply.github.com>
2025-07-31 02:57:19 +0300

    Changed the GetRegister(str) method for the IVirtualCPU

--- src/EmuXCore/VM/Interfaces/Components/IVirtualCPU.cs
+++ src/EmuXCore/VM/Interfaces/Components/IVirtualCPU.cs
@@ -11,16 +11,17 @@ public interface IVirtualCPU : IVirtualComponent
     /// Finds the specified register and returns it as type T
     /// </summary>
     /// <typeparam name="T">The register to cast it as</typeparam>
-    /// <returns>The specified register caster as T</returns>
     /// <exception cref="KeyNotFoundException">If the register is not found then an exception is raised</exception>
+    /// <returns>The specified register caster as T</returns>
     T GetRegister<T>() where T : IVirtualRegister;
 
     /// <summary>
     /// Finds the specified register and returns it as IVirtualRegister
     /// </summary>
     /// <param name="registerName">The register name, doesn't matter if it is AL or RAX or if it uppercase or lowercase, it will return the accumulator</param>
-    /// <returns>The register if found, otherwise null</returns>
-    IVirtualRegister? GetRegister(string registerName);
+    /// <exception cref="KeyNotFoundException">If the register is not found then an exception is raised</exception>
+    /// <returns>The specified register</returns>
+    IVirtualRegister GetRegister(string registerName);
 
     /// <summary>
     /// All of the registers of the VCPU
