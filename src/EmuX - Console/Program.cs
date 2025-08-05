d8b1a21b00861da1d5b5862ad4854c7a023bcea8
RysteQ <34135554+RysteQ@users.noreply.github.com>
2025-08-05 14:32:33 +0300

    Just pushing all of my progress with the TUI for now

--- src/EmuX - Console/Program.cs
+++ src/EmuX - Console/Program.cs
@@ -1,34 +1,19 @@
 ﻿using EmuX_Console.Libraries;
 using EmuX_Console.Libraries.Enums;
 using EmuX_Console.Libraries.Interfaces;
-using EmuXCore.VM;
-using EmuXCore.VM.Interfaces;
-using EmuXCore.VM.Internal.BIOS;
-using EmuXCore.VM.Internal.BIOS.Internal;
-using EmuXCore.VM.Internal.CPU;
-using EmuXCore.VM.Internal.Device.USBDrives;
-using EmuXCore.VM.Internal.Disk;
-using EmuXCore.VM.Internal.GPUs;
-using EmuXCore.VM.Internal.Memory;
-using EmuXCore.VM.Internal.RTC;
+using EmuXCore.VM.Internal.CPU.Registers.MainRegisters;
 
 string input = string.Empty;
 ITerminalIOHandler terminalIOHandler = new TerminalIOHandler("> ", ConsoleKey.UpArrow, ConsoleKey.DownArrow);
-IVirtualMachineBuilder virtualMachineBuilder = new VirtualMachineBuilder();
-IVirtualMachine virtualMachine = virtualMachineBuilder
-            .SetCpu(new VirtualCPU())
-            .SetMemory(new VirtualMemory())
-            .SetBios(new VirtualBIOS(new DiskInterruptHandler(), new RTCInterruptHandler(), new VideoInterruptHandler(), new DeviceInterruptHandler()))
-            .SetRTC(new VirtualRTC())
-            .AddDisk(new VirtualDisk(1, 4, 16, 64))
-            .AddDisk(new VirtualDisk(2, 4, 12, 24))
-            .SetGPU(new VirtualGPU())
-            .AddVirtualDevice(new UsbDrive64Kb(1))
-            .Build();
 
 Console.Clear();
 
-terminalIOHandler.Output(virtualMachine);
+terminalIOHandler.Output([255, 69, 0], OutputFormat.Integer, OutputSeverity.Normal);
+terminalIOHandler.Output([255, 69, 0], OutputFormat.Binary, OutputSeverity.Normal);
+terminalIOHandler.Output([255, 69, 0], OutputFormat.Hexadecimal, OutputSeverity.Normal);
+terminalIOHandler.Output(new VirtualRegisterRAX(), OutputFormat.Integer);
+terminalIOHandler.Output(new VirtualRegisterRAX(), OutputFormat.Binary);
+terminalIOHandler.Output(new VirtualRegisterRAX(), OutputFormat.Hexadecimal);
 
 while (true)
 {
