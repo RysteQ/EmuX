e2595d58a46e56406003b532662e73c41378c924
640 XDM <640xdm460ds4@gmail.com>
2025-08-19 06:55:05 +0300

    Added automated calculation of the ID in the constructor of the InstructionVariant

--- src/EmuXCore/InstructionLogic/Instructions/Internal/InstructionVariant.cs
+++ src/EmuXCore/InstructionLogic/Instructions/Internal/InstructionVariant.cs
@@ -4,32 +4,36 @@ namespace EmuXCore.InstructionLogic.Instructions.Internal;
 
 public class InstructionVariant : IComparable
 {
-    public InstructionVariant(int id, OperandVariant? firstOperand = null, OperandVariant? secondOperand = null, OperandVariant? thirdOperand = null)
+    public InstructionVariant(OperandVariant? firstOperand = null, OperandVariant? secondOperand = null, OperandVariant? thirdOperand = null)
     {
-        Id = id;
         FirstOperand = firstOperand;
         SecondOperand = secondOperand;
         ThirdOperand = thirdOperand;
+    
+        firstOperand = firstOperand == null ? OperandVariant.NaN : firstOperand;
+        secondOperand = secondOperand == null ? OperandVariant.NaN : secondOperand;
+        thirdOperand = thirdOperand == null ? OperandVariant.NaN : thirdOperand;
+
+        Id = (int)firstOperand * 16 + (int)secondOperand * 4 + (int)thirdOperand;
     }
 
-    public static InstructionVariant NoOperands() => new(1);
+    public static InstructionVariant NoOperands() => new();
 
-    public static InstructionVariant OneOperandValue() => new(2, OperandVariant.Value);
-    public static InstructionVariant OneOperandRegister() => new(3, OperandVariant.Register);
-    public static InstructionVariant OneOperandMemory() => new(4, OperandVariant.Memory);
-    public static InstructionVariant OneOperandLabel() => new(13, OperandVariant.Label);
+    public static InstructionVariant OneOperandValue() => new(OperandVariant.Value);
+    public static InstructionVariant OneOperandRegister() => new(OperandVariant.Register);
+    public static InstructionVariant OneOperandMemory() => new(OperandVariant.Memory);
 
-    public static InstructionVariant TwoOperandsRegisterValue() => new(5, OperandVariant.Register, OperandVariant.Value);
-    public static InstructionVariant TwoOperandsRegisterRegister() => new(6, OperandVariant.Register, OperandVariant.Register);
-    public static InstructionVariant TwoOperandsRegisterMemory() => new(7, OperandVariant.Register, OperandVariant.Memory);
-    public static InstructionVariant TwoOperandsMemoryValue() => new(8, OperandVariant.Memory, OperandVariant.Value);
-    public static InstructionVariant TwoOperandsMemoryRegister() => new(9, OperandVariant.Memory, OperandVariant.Register);
-    public static InstructionVariant TwoOperandsValueRegister() => new(10, OperandVariant.Value, OperandVariant.Register);
+    public static InstructionVariant TwoOperandsRegisterValue() => new(OperandVariant.Register, OperandVariant.Value);
+    public static InstructionVariant TwoOperandsRegisterRegister() => new(OperandVariant.Register, OperandVariant.Register);
+    public static InstructionVariant TwoOperandsRegisterMemory() => new(OperandVariant.Register, OperandVariant.Memory);
+    public static InstructionVariant TwoOperandsMemoryValue() => new(OperandVariant.Memory, OperandVariant.Value);
+    public static InstructionVariant TwoOperandsMemoryRegister() => new(OperandVariant.Memory, OperandVariant.Register);
+    public static InstructionVariant TwoOperandsValueRegister() => new(OperandVariant.Value, OperandVariant.Register);
 
-    public static InstructionVariant ThreeOperandsRegisterRegisterValue() => new(11, OperandVariant.Register, OperandVariant.Register, OperandVariant.Value);
-    public static InstructionVariant ThreeOperandsRegisterMemoryValue() => new(12, OperandVariant.Register, OperandVariant.Memory, OperandVariant.Value);
+    public static InstructionVariant ThreeOperandsRegisterRegisterValue() => new(OperandVariant.Register, OperandVariant.Register, OperandVariant.Value);
+    public static InstructionVariant ThreeOperandsRegisterMemoryValue() => new(OperandVariant.Register, OperandVariant.Memory, OperandVariant.Value);
 
-    public static InstructionVariant NaN() => new(-1);
+    public static InstructionVariant NaN() => new() { Id = -1 };
 
 #pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
     public int CompareTo(object? obj) => Id.CompareTo(((InstructionVariant)obj).Id);
