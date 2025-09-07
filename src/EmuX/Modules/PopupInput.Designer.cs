32e6a3e306ef12d4a55f9c8a111b58f79d03b858
640 XDM <640xdm460ds4@gmail.com>
2025-09-07 04:29:10 +0300

    Added a new popup window element

---
+++ src/EmuX/Modules/PopupInput.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace EmuX_Nano.Modules
+{
+    partial class PopupInput
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelQuestion = new Label();
+            textBoxUserInput = new TextBox();
+            SuspendLayout();
+            // 
+            // labelQuestion
+            // 
+            labelQuestion.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            labelQuestion.AutoSize = true;
+            labelQuestion.Location = new Point(12, 9);
+            labelQuestion.Name = "labelQuestion";
+            labelQuestion.Size = new Size(32, 15);
+            labelQuestion.TabIndex = 0;
+            labelQuestion.Text = "label";
+            // 
+            // textBoxUserInput
+            // 
+            textBoxUserInput.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBoxUserInput.Location = new Point(12, 33);
+            textBoxUserInput.Name = "textBoxUserInput";
+            textBoxUserInput.Size = new Size(157, 23);
+            textBoxUserInput.TabIndex = 1;
+            textBoxUserInput.KeyPress += textBoxUserInput_KeyPress;
+            // 
+            // PopupInput
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(177, 65);
+            Controls.Add(textBoxUserInput);
+            Controls.Add(labelQuestion);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "PopupInput";
+            TopMost = true;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label labelQuestion;
+        private TextBox textBoxUserInput;
+    }
+}
---
+++ src/EmuX/Modules/PopupInput.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+
+namespace EmuX_Nano.Modules;
+
+public partial class PopupInput : Form
+{
+    public PopupInput()
+    {
+        InitializeComponent();
+    }
+
+    private void textBoxUserInput_KeyPress(object sender, KeyPressEventArgs e)
+    {
+        if (e.KeyChar != ENTER_ASCII_VALUE)
+        {
+            return;
+        }
+
+        if (!string.IsNullOrEmpty(textBoxUserInput.Text))
+        {
+            _userInput = textBoxUserInput.Text;
+            this.Close();
+        }
+    }
+
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+    public string Question { get => labelQuestion.Text; set => labelQuestion.Text = value; }
+    public string? UserInput { get => _userInput; }
+
+    private string? _userInput = null;
+
+    private const byte ENTER_ASCII_VALUE = 13;
+}
