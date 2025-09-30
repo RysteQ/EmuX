0e80feba323102c2ba98691ed7791cde480a15c5
640 XDM <640xdm460ds4@gmail.com>
2025-10-01 02:49:31 +0300

    Finished the IVmAction implementation

--- src/EmuXCore/VM/Actions/VmAction.cs
+++ src/EmuXCore/VM/Actions/VmAction.cs
@@ -2,6 +2,9 @@
 using EmuXCore.VM.Enums;
 using EmuXCore.VM.Interfaces;
 using EmuXCore.VM.Interfaces.Actions;
+using EmuXCore.VM.Interfaces.Components;
+using EmuXCore.VM.Interfaces.Components.Internal;
+using EmuXCore.VM.Internal.Disk;
 
 namespace EmuXCore.VM.Actions;
 
@@ -21,20 +24,230 @@ public class VmAction : IVmAction
 
     public void Undo(IVirtualMachine virtualMachineInstance)
     {
-        // TODO
+        switch (Action)
+        {
+            case VmActionCategory.ModifiedRegister: UndoRegisterAction(virtualMachineInstance); break;
+            case VmActionCategory.ModifiedMemory: UndoMemoryAction(virtualMachineInstance); break;
+            case VmActionCategory.ModifiedDisk: UndoDiskAction(virtualMachineInstance); break;
+            case VmActionCategory.ModifiedDevice: UndoDeviceAction(virtualMachineInstance); break;
+            case VmActionCategory.ModifiedGpu: UndoGpuAction(virtualMachineInstance); break;
+            default: throw new ArgumentException("Invalid action");
+        }
     }
 
     public void Redo(IVirtualMachine virtualMachineInstance)
     {
-        // TODO
+        switch (Action)
+        {
+            case VmActionCategory.ModifiedRegister: RedoRegisterAction(virtualMachineInstance); break;
+            case VmActionCategory.ModifiedMemory: RedoMemoryAction(virtualMachineInstance); break;
+            case VmActionCategory.ModifiedDisk: RedoDiskAction(virtualMachineInstance); break;
+            case VmActionCategory.ModifiedDevice: RedoDeviceAction(virtualMachineInstance); break;
+            case VmActionCategory.ModifiedGpu: RedoGpuAction(virtualMachineInstance); break;
+            default: throw new ArgumentException("Invalid action");
+        }
+    }
+
+    private void UndoRegisterAction(IVirtualMachine virtualMachineInstance)
+    {
+        IVirtualRegister? virtualRegister = null;
+        ulong currentRegisterValue = 0;
+
+        if (string.IsNullOrEmpty(RegisterName))
+        {
+            throw new ArgumentNullException($"Value {nameof(RegisterName)} is null or empty");
+        }
+
+        virtualRegister = virtualMachineInstance.CPU.GetRegister(RegisterName);
+        currentRegisterValue = virtualRegister.Get();
+
+        switch (Size)
+        {
+            case Size.Byte: virtualRegister.Set((currentRegisterValue | 0x_ff_ff_ff_ff_ff_ff_ff_00) + ConvertBytesToUlong(PreviousValue)); break;
+            case Size.Word: virtualRegister.Set((currentRegisterValue | 0x_ff_ff_ff_ff_ff_ff_00_00) + ConvertBytesToUlong(PreviousValue)); break;
+            case Size.Dword: virtualRegister.Set((currentRegisterValue | 0x_ff_ff_ff_ff_00_00_00_00) + ConvertBytesToUlong(PreviousValue)); break;
+            case Size.Qword: virtualRegister.Set(ConvertBytesToUlong(PreviousValue)); break;
+            default: throw new ArgumentException("Invalid size");
+        }
+    }
+
+    private void UndoMemoryAction(IVirtualMachine virtualMachineInstance)
+    {
+        if (MemoryPointer == null)
+        {
+            throw new ArgumentNullException($"Value {nameof(MemoryPointer)} is null or empty");
+        }
+
+        for (int i = 0; i < PreviousValue.Length; i++)
+        {
+            virtualMachineInstance.SetByte((int)(MemoryPointer + i), PreviousValue[i]);
+        }
+    }
+
+    private void UndoDiskAction(IVirtualMachine virtualMachineInstance)
+    {
+        IVirtualDisk? virtualDisk = virtualMachineInstance.Disks.FirstOrDefault(selectedDisk => selectedDisk.DiskNumber == DiskId);
+
+        if (MemoryPointer == null)
+        {
+            throw new ArgumentNullException($"Value {nameof(MemoryPointer)} is null or empty");
+        }
+
+        if (virtualDisk == null)
+        {
+            throw new ArgumentNullException($"Cannot find the disk with the ID of {DiskId}");
+        }
+
+        for (int i = 0; i < PreviousValue.Length; i++)
+        {
+            virtualDisk.WriteSector((byte)((MemoryPointer & 0x_ff_00_00_00) >> 24), (ushort)((MemoryPointer & 0x_00_ff_ff_00) >> 8), (byte)(MemoryPointer & 0x_00_00_00_ff), PreviousValue);
+        }
+    }
+
+    private void UndoDeviceAction(IVirtualMachine virtualMachineInstance)
+    {
+        IVirtualDevice? virtualDevice = virtualMachineInstance.Devices.FirstOrDefault(selectedDevice => selectedDevice.DeviceId == DeviceId);
+
+        if (MemoryPointer == null)
+        {
+            throw new ArgumentNullException($"Value {nameof(MemoryPointer)} is null or empty");
+        }
+
+        if (virtualDevice == null)
+        {
+            throw new ArgumentNullException($"Cannot find the device with the ID of {DeviceId}");
+        }
+
+        for (int i = 0; i < PreviousValue.Length; i++)
+        {
+            virtualDevice.Data[(int)(MemoryPointer + i)] = PreviousValue[i];
+        }
+    }
+
+    private void UndoGpuAction(IVirtualMachine virtualMachineInstance)
+    {
+        IVirtualGPU virtualGPU = virtualMachineInstance.GPU;
+
+        if (MemoryPointer == null)
+        {
+            throw new ArgumentNullException($"Value {nameof(MemoryPointer)} is null or empty");
+        }
+
+        for (int i = 0; i < PreviousValue.Length; i++)
+        {
+            virtualGPU.Data[(int)(MemoryPointer + i)] = PreviousValue[i];
+        }
+    }
+
+    private void RedoRegisterAction(IVirtualMachine virtualMachineInstance)
+    {
+        IVirtualRegister? virtualRegister = null;
+        ulong currentRegisterValue = 0;
+
+        if (string.IsNullOrEmpty(RegisterName))
+        {
+            throw new ArgumentNullException($"Value {nameof(RegisterName)} is null or empty");
+        }
+
+        virtualRegister = virtualMachineInstance.CPU.GetRegister(RegisterName);
+        currentRegisterValue = virtualRegister.Get();
+
+        switch (Size)
+        {
+            case Size.Byte: virtualRegister.Set((currentRegisterValue | 0x_ff_ff_ff_ff_ff_ff_ff_00) + ConvertBytesToUlong(NewValue)); break;
+            case Size.Word: virtualRegister.Set((currentRegisterValue | 0x_ff_ff_ff_ff_ff_ff_00_00) + ConvertBytesToUlong(NewValue)); break;
+            case Size.Dword: virtualRegister.Set((currentRegisterValue | 0x_ff_ff_ff_ff_00_00_00_00) + ConvertBytesToUlong(NewValue)); break;
+            case Size.Qword: virtualRegister.Set(ConvertBytesToUlong(NewValue)); break;
+            default: throw new ArgumentException("Invalid size");
+        }
+    }
+
+    private void RedoMemoryAction(IVirtualMachine virtualMachineInstance)
+    {
+        if (MemoryPointer == null)
+        {
+            throw new ArgumentNullException($"Value {nameof(MemoryPointer)} is null or empty");
+        }
+
+        for (int i = 0; i < PreviousValue.Length; i++)
+        {
+            virtualMachineInstance.SetByte((int)(MemoryPointer + i), NewValue[i]);
+        }
+    }
+
+    private void RedoDiskAction(IVirtualMachine virtualMachineInstance)
+    {
+        IVirtualDisk? virtualDisk = virtualMachineInstance.Disks.FirstOrDefault(selectedDisk => selectedDisk.DiskNumber == DiskId);
+
+        if (MemoryPointer == null)
+        {
+            throw new ArgumentNullException($"Value {nameof(MemoryPointer)} is null or empty");
+        }
+
+        if (virtualDisk == null)
+        {
+            throw new ArgumentNullException($"Cannot find the disk with the ID of {DiskId}");
+        }
+
+        for (int i = 0; i < NewValue.Length; i++)
+        {
+            virtualDisk.WriteSector((byte)((MemoryPointer & 0x_ff_00_00_00) >> 24), (ushort)((MemoryPointer & 0x_00_ff_ff_00) >> 8), (byte)(MemoryPointer & 0x_00_00_00_ff), NewValue);
+        }
+    }
+
+    private void RedoDeviceAction(IVirtualMachine virtualMachineInstance)
+    {
+        IVirtualDevice? virtualDevice = virtualMachineInstance.Devices.FirstOrDefault(selectedDevice => selectedDevice.DeviceId == DeviceId);
+
+        if (MemoryPointer == null)
+        {
+            throw new ArgumentNullException($"Value {nameof(MemoryPointer)} is null or empty");
+        }
+
+        if (virtualDevice == null)
+        {
+            throw new ArgumentNullException($"Cannot find the device with the ID of {DeviceId}");
+        }
+
+        for (int i = 0; i < NewValue.Length; i++)
+        {
+            virtualDevice.Data[(int)(MemoryPointer + i)] = NewValue[i];
+        }
+    }
+
+    private void RedoGpuAction(IVirtualMachine virtualMachineInstance)
+    {
+        IVirtualGPU virtualGPU = virtualMachineInstance.GPU;
+
+        if (MemoryPointer == null)
+        {
+            throw new ArgumentNullException($"Value {nameof(MemoryPointer)} is null or empty");
+        }
+
+        for (int i = 0; i < NewValue.Length; i++)
+        {
+            virtualGPU.Data[(int)(MemoryPointer + i)] = NewValue[i];
+        }
+    }
+
+    private ulong ConvertBytesToUlong(byte[] bytesToConvert)
+    {
+        switch (Size)
+        {
+            case Size.Byte: return bytesToConvert[0];
+            case Size.Word: return ((ulong)bytesToConvert[0] << 8) + (ulong)bytesToConvert[1];
+            case Size.Dword: return ((ulong)bytesToConvert[0] << 24) + ((ulong)bytesToConvert[1] << 16) + ((ulong)bytesToConvert[2] << 8) + (ulong)bytesToConvert[3];
+            case Size.Qword: return ((ulong)bytesToConvert[0] << 56) + ((ulong)bytesToConvert[1] << 48) + ((ulong)bytesToConvert[2] << 40) + ((ulong)bytesToConvert[3] << 32) + ((ulong)bytesToConvert[4] << 24) + ((ulong)bytesToConvert[5] << 16) + ((ulong)bytesToConvert[6] << 8) + (ulong)bytesToConvert[7];
+            default: return 0;
+        }
     }
 
     public VmActionCategory Action { get; init; }
     public Size Size { get; init; }
     public string? RegisterName { get; init; }
     public int? MemoryPointer { get; init; }
-    public int? DeviceId { get; init; }
-    public byte? DiskId { get; init; }
+    public int? DeviceId { get; set; }
+    public byte? DiskId { get; set; }
     public byte[] PreviousValue { get; init; }
     public byte[] NewValue { get; init; }
 }
