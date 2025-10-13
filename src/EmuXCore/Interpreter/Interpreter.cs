7c3f3f51c90500ab565b4b61758e7b89d1f05cf6
640 XDM <640xdm460ds4@gmail.com>
2025-10-13 15:16:07 +0300

    Implemented the implementation of the IInterpreter interface

--- src/EmuXCore/Interpreter/Interpreter.cs
+++ src/EmuXCore/Interpreter/Interpreter.cs
@@ -2,32 +2,139 @@
 using EmuXCore.Interpreter.Interfaces;
 using EmuXCore.Interpreter.Models.Interfaces;
 using EmuXCore.VM.Interfaces;
+using EmuXCore.VM.Interfaces.Actions;
 
 namespace EmuXCore.Interpreter;
 
 public class Interpreter : IInterpreter
 {
-    // TODO
-    public IVirtualMachine VirtualMachine { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-    public IList<IInstruction> Instructions { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-    public IList<ILabel> Labels { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-    public IInstruction CurrentInstruction => throw new NotImplementedException();
-
-    public int CurrentInstructionIndex => throw new NotImplementedException();
-
     public void Execute()
     {
-        throw new NotImplementedException();
+        foreach (IInstruction instruction in Instructions)
+        {
+            ExecuteStep();
+        }
     }
 
     public void ExecuteStep()
     {
-        throw new NotImplementedException();
+        if (!_instructions.Any())
+        {
+            return;
+        }
+
+        _instructions[_currentInstructionIndex].Execute(_virtualMachine);
+
+        _actions.Add(_currentInstructionIndex, [.. _virtualMachine.Actions.TakeLast(_virtualMachine.Actions.Count - _actions.Values.Sum(selectedRecord => selectedRecord.Count))]);
+        _currentInstructionIndex++;
+    }
+
+    public void UndoAction()
+    {
+        if (_currentInstructionIndex == 0)
+        {
+            return;
+        }
+
+        for (int i = _actions[_currentInstructionIndex - 1].Count - 1; i >= 0; i--)
+        {
+            _actions[_currentInstructionIndex - 1][i].Undo(_virtualMachine);
+        }
+
+        _currentInstructionIndex--;
+    }
+
+    public void UndoActions(int actions)
+    {
+        for (int i = 0; i < actions; i++)
+        {
+            UndoAction();
+        }
+    }
+
+    public void RedoAction()
+    {
+        if (_currentInstructionIndex == _instructions.Count)
+        {
+            return;
+        }
+
+
+        for (int i = _actions[_currentInstructionIndex].Count - 1; i >= 0; i--)
+        {
+            _actions[_currentInstructionIndex][i].Redo(_virtualMachine);
+        }
+
+        _currentInstructionIndex++;
+    }
+
+    public void RedoActions(int actions)
+    {
+        for (int i = 0; i < actions; i++)
+        {
+            RedoAction();
+        }
     }
 
     public void ResetExecution()
     {
-        throw new NotImplementedException();
+        for (int i = _virtualMachine.Actions.Count - 1; i >= 0; i--)
+        {
+            _virtualMachine.Actions[i].Undo(_virtualMachine);
+        }
+
+        _virtualMachine.Actions.Clear();
+        _actions.Clear();
+        _currentInstructionIndex = 0;
     }
+
+    private void ConfigureCriticalProperties()
+    {
+        if (_virtualMachine == null)
+        {
+            throw new ArgumentNullException($"Property {nameof(VirtualMachine)} cannot be null");
+        }
+
+        _actions.Clear();
+        _currentInstructionIndex = 0;
+    }
+
+    public IVirtualMachine VirtualMachine
+    {
+        get => _virtualMachine;
+        set
+        {
+            _virtualMachine = value;
+            ConfigureCriticalProperties();
+        }
+    }
+
+    public IList<IInstruction> Instructions
+    {
+        get => _instructions;
+        set
+        {
+            _instructions = value;
+            ConfigureCriticalProperties();
+        }
+    }
+
+    public IList<ILabel> Labels
+    {
+        get => _labels;
+        set
+        {
+            _labels = value;
+            ConfigureCriticalProperties();
+        }
+    }
+
+    public IInstruction CurrentInstruction => Instructions[_currentInstructionIndex];
+    public int CurrentInstructionIndex => _currentInstructionIndex;
+
+    private IVirtualMachine _virtualMachine;
+    private IList<IInstruction> _instructions;
+    private IList<ILabel> _labels;
+    private Dictionary<int, List<IVmAction>> _actions = [];
+    private int _currentInstructionIndex;
 }
