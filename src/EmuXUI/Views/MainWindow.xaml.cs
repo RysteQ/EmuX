90b440b30f71f741a48bada407ec5924c655a384
RysteQ <34135554+RysteQ@users.noreply.github.com>
2025-12-22 14:50:32 +0200

    Added all major functionality that does not require a new window to show up like the syntax highlighting settings window or the font settings window

--- src/EmuXUI/Views/MainWindow.xaml.cs
+++ src/EmuXUI/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using EmuXUI.Assets.SyntaxHighlighting;
+using EmuXUI.Models.Static;
 using EmuXUI.Popups;
 using EmuXUI.ViewModels;
 using Microsoft.UI.Input;
@@ -9,7 +10,6 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using Microsoft.Windows.Storage.Pickers;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -20,8 +20,12 @@ using System.Threading.Tasks;
 using TextControlBoxNS;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.PlayTo;
 using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.System;
 using Windows.UI;
+using Windows.UI.Popups;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -93,49 +97,126 @@ public sealed partial class MainWindow : Window
         ContentDialogResult result = await noWifiDialog.ShowAsync();
     }
 
-    private void MenuFlyoutItemOpenFile_Click(object sender, RoutedEventArgs e)
+    private async void MenuFlyoutItemOpenFile_Click(object sender, RoutedEventArgs e)
     {
+        FileOpenPicker filePicker = new();
+        StorageFile pickedFile;
 
+        // Why do I have to do this Microsoft ?
+        WinRT.Interop.InitializeWithWindow.Initialize(filePicker, System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle);
+
+        filePicker.SuggestedStartLocation = PickerLocationId.Desktop;
+        filePicker.ViewMode = PickerViewMode.List;
+        filePicker.FileTypeFilter.Add(".asm");
+        filePicker.FileTypeFilter.Add(".txt");
+
+        pickedFile = await filePicker.PickSingleFileAsync();
+
+        if (pickedFile == null)
+        {
+            return;
+        }
+
+        _selectedFile = pickedFile.Path;
+        SourceCodeTextControlBox.Text = File.ReadAllText(_selectedFile);
     }
 
-    private void MenuFlyoutItemSaveFile_Click(object sender, RoutedEventArgs e)
+    private async void MenuFlyoutItemSaveFile_Click(object sender, RoutedEventArgs e)
     {
+        if (string.IsNullOrEmpty(_selectedFile))
+        {
+            MenuFlyoutItemSaveFileAs_Click(sender, e);
 
+            return;
+        }
+
+        File.WriteAllText(_selectedFile, SourceCodeTextControlBox.Text);
     }
 
-    private void MenuFlyoutItemSaveFileAs_Click(object sender, RoutedEventArgs e)
+    private async void MenuFlyoutItemSaveFileAs_Click(object sender, RoutedEventArgs e)
     {
+        FileSavePicker savePicker = new();
+        StorageFile saveLocation;
 
+        WinRT.Interop.InitializeWithWindow.Initialize(savePicker, System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle);
+
+        savePicker.SuggestedStartLocation = PickerLocationId.Desktop;
+        savePicker.FileTypeChoices.Add("Assembly", [".asm"]);
+        savePicker.FileTypeChoices.Add("Text file", [".txt"]);
+
+        saveLocation = await savePicker.PickSaveFileAsync();
+
+        if (saveLocation == null)
+        {
+            new InfoPopup(Enums.InfoPopupSeverity.Warning, "The file wasn't saved, a valid save location is required").Activate();
+
+            return;
+        }
+
+        _selectedFile = saveLocation.Path;
+        File.WriteAllText(_selectedFile, SourceCodeTextControlBox.Text);
     }
 
     private void MenuFlyoutItemUndo_Click(object sender, RoutedEventArgs e)
     {
-
+        SourceCodeTextControlBox.Undo();
     }
 
     private void MenuFlyoutItemRedo_Click(object sender, RoutedEventArgs e)
     {
-
+        SourceCodeTextControlBox.Redo();
     }
 
     private void MenuFlyoutItemFind_Click(object sender, RoutedEventArgs e)
     {
+        GridFindStringReferenceControlsGroup.Visibility = Visibility.Visible;
+        ButtonFindSingleReferenceToString.Visibility = Visibility.Visible;
+        ButtonFindAllReferencesToString.Visibility = Visibility.Visible;
+        StackPanelReplaceStringControlsGroup.Visibility = Visibility.Collapsed;
 
+        _findModeEnabled = true;
+        _findAllStringReferencesClicked = false;
+
+        TextBoxFindString.Focus(FocusState.Keyboard);
     }
 
     private void MenuFlyoutItemFindAll_Click(object sender, RoutedEventArgs e)
     {
+        GridFindStringReferenceControlsGroup.Visibility = Visibility.Visible;
+        ButtonFindSingleReferenceToString.Visibility = Visibility.Visible;
+        ButtonFindAllReferencesToString.Visibility = Visibility.Visible;
+        StackPanelReplaceStringControlsGroup.Visibility = Visibility.Collapsed;
 
+        _findModeEnabled = true;
+        _findAllStringReferencesClicked = true;
+
+        TextBoxFindString.Focus(FocusState.Keyboard);
     }
 
     private void MenuFlyoutItemReplace_Click(object sender, RoutedEventArgs e)
     {
+        GridFindStringReferenceControlsGroup.Visibility = Visibility.Visible;
+        ButtonFindSingleReferenceToString.Visibility = Visibility.Collapsed;
+        ButtonFindAllReferencesToString.Visibility = Visibility.Collapsed;
+        StackPanelReplaceStringControlsGroup.Visibility = Visibility.Visible;
 
+        _findModeEnabled = false;
+        _replaceAllStringReferencesClicked = false;
+
+        TextBoxFindString.Focus(FocusState.Keyboard);
     }
 
     private void MenuFlyoutItemReplaceAll_Click(object sender, RoutedEventArgs e)
     {
+        GridFindStringReferenceControlsGroup.Visibility = Visibility.Visible;
+        ButtonFindSingleReferenceToString.Visibility = Visibility.Collapsed;
+        ButtonFindAllReferencesToString.Visibility = Visibility.Collapsed;
+        StackPanelReplaceStringControlsGroup.Visibility = Visibility.Visible;
 
+        _findModeEnabled = false;
+        _replaceAllStringReferencesClicked = true;
+
+        TextBoxFindString.Focus(FocusState.Keyboard);
     }
 
     private void MenuFlyoutItemFontSettings_Click(object sender, RoutedEventArgs e)
@@ -150,12 +231,136 @@ public sealed partial class MainWindow : Window
 
     private void SourceCodeTextControlBox_PointerPressed(object sender, PointerRoutedEventArgs e)
     {
+        if (GridFindStringReferenceControlsGroup.Visibility == Visibility.Visible)
+        {
+            GridFindStringReferenceControlsGroup.Visibility = Visibility.Collapsed;
+        }
+
         UpdateSizeRowColumnTextBlocks();
     }
 
     private void SourceCodeTextControlBox_KeyDown(object sender, KeyRoutedEventArgs e)
     {
+        if (GridFindStringReferenceControlsGroup.Visibility == Visibility.Visible)
+        {
+            GridFindStringReferenceControlsGroup.Visibility = Visibility.Collapsed;
+            SourceCodeTextControlBox.EndSearch();
+        }
+
         UpdateSizeRowColumnTextBlocks();
+    }
+    
+    private void TextBoxFindString_KeyDown(object sender, KeyRoutedEventArgs e)
+    {
+        if (e.Key != VirtualKey.Enter)
+        {
+            return;
+        }
+
+        if (_findModeEnabled)
+        {
+            if (_findAllStringReferencesClicked)
+            {
+                FindAllTextReferences();
+            }
+            else
+            {
+                FindAndNavigateToTextReference();
+            }
+        }
+        else
+        {
+            TextBoxReplaceString.Focus(FocusState.Keyboard);
+        }
+    }
+
+    private void TextBoxReplaceString_KeyDown(object sender, KeyRoutedEventArgs e)
+    {
+        if (e.Key != VirtualKey.Enter || _findModeEnabled)
+        {
+            return;
+        }
+
+        if (_replaceAllStringReferencesClicked)
+        {
+            ReplaceAllTextReferences();
+        }
+        else
+        {
+            ReplaceNextTextReference();
+        }
+    }
+
+    private void ButtonFindSingleReferenceToString_Click(object sender, RoutedEventArgs e)
+    {
+        _findAllStringReferencesClicked = false;
+
+        FindAndNavigateToTextReference();
+    }
+
+    private void ButtonFindAllReferencesToString_Click(object sender, RoutedEventArgs e)
+    {
+        _findAllStringReferencesClicked = true;
+
+        FindAllTextReferences();
+    }
+
+    private void FindAndNavigateToTextReference()
+    {
+        string[] sourceCodeLines = SourceCodeTextControlBox.Text.Split('\n');
+        StringComparison comparison = StringComparison.OrdinalIgnoreCase;
+
+        if (string.IsNullOrEmpty(TextBoxFindString.Text))
+        {
+            Task.Run(() =>
+            {
+                Console.Beep();
+            });
+
+            return;
+        }
+
+        if (CheckboxMatchStringReferenceToLookupCase.IsChecked == true)
+        {
+            comparison = StringComparison.Ordinal;
+        }
+
+        for (int i = SourceCodeTextControlBox.CurrentLineIndex; i < sourceCodeLines.Length; i++)
+        {
+            if (sourceCodeLines[i].Contains(TextBoxFindString.Text, comparison))
+            {
+                GridFindStringReferenceControlsGroup.Visibility = Visibility.Collapsed;
+
+                SourceCodeTextControlBox.SetCursorPosition(i, sourceCodeLines[i].IndexOf(TextBoxFindString.Text));
+                SourceCodeTextControlBox.Focus(FocusState.Keyboard);
+
+                return;
+            }
+        }
+
+        Task.Run(() =>
+        {
+            Console.Beep();
+        });
+    }
+
+    private void FindAllTextReferences()
+    {
+        SourceCodeTextControlBox.BeginSearch(TextBoxFindString.Text, false, CheckboxMatchStringReferenceToLookupCase.IsChecked == true);
+    }
+
+    private void ReplaceNextTextReference()
+    {
+        SourceCodeTextControlBox.BeginSearch(TextBoxFindString.Text, false, CheckboxMatchStringReferenceToLookupCase.IsChecked == true);
+        SourceCodeTextControlBox.ReplaceNext(TextBoxReplaceString.Text);
+        SourceCodeTextControlBox.EndSearch();
+    }
+
+    private void ReplaceAllTextReferences()
+    {
+        SourceCodeTextControlBox.BeginSearch(TextBoxFindString.Text, false, CheckboxMatchStringReferenceToLookupCase.IsChecked == true);
+        SourceCodeTextControlBox.ReplaceAll(TextBoxFindString.Text, TextBoxReplaceString.Text, CheckboxMatchStringReferenceToLookupCase.IsChecked == true, false);
+        SourceCodeTextControlBox.EndSearch();
     }
 
     private void UpdateSizeRowColumnTextBlocks()
@@ -166,4 +371,9 @@ public sealed partial class MainWindow : Window
     }
 
     public MainWindowViewModel ViewModel { get; set; }
+
+    private string _selectedFile = string.Empty;
+    private bool _findModeEnabled = false;
+    private bool _findAllStringReferencesClicked = false;
+    private bool _replaceAllStringReferencesClicked = false;
 }
