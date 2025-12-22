bd29fff999da1841cff1f447f8a47f25c7ae0a7b
RysteQ <34135554+RysteQ@users.noreply.github.com>
2025-12-22 05:20:19 +0200

    Added a custom info popup view

---
+++ src/EmuXUI/Popups/InfoPopup.xaml.cs
@@ -0,0 +1,57 @@
+using EmuXUI.Enums;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+
+namespace EmuXUI.Popups;
+
+public sealed partial class InfoPopup : Window
+{
+    public InfoPopup(InfoPopupSeverity severity, string message)
+    {
+        InitializeComponent();
+
+        InitUI();
+        InitialisePopupContents(severity, message);
+    }
+
+    private void InitUI()
+    {
+        ExtendsContentIntoTitleBar = true;
+        SetTitleBar(TitleBarGrid);
+
+        AppWindow.Resize(new((int)mainGrid.MaxWidth, (int)mainGrid.MaxHeight));
+
+        DisableWindowResizing();
+    }
+
+    private void DisableWindowResizing()
+    {
+        nint windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(this);
+        WindowId windowId = Win32Interop.GetWindowIdFromWindow(windowHandle);
+        AppWindow? applicationWindow = AppWindow.GetFromWindowId(windowId);
+        OverlappedPresenter? presenter = applicationWindow?.Presenter as OverlappedPresenter;
+
+        if (presenter == null)
+        {
+            return;
+        }
+
+        presenter.IsResizable = false;
+        presenter.IsMaximizable = false;
+    }
+
+
+    private void InitialisePopupContents(InfoPopupSeverity severity, string message)
+    {
+        textBlockInfoTitle.Text = severity switch
+        {
+            InfoPopupSeverity.Info => "Info",
+            InfoPopupSeverity.Warning => "Warning",
+            InfoPopupSeverity.Error => "Error",
+            _ => "Info"
+        };
+
+        textBlockInfoContents.Text = message;
+    }
+}
