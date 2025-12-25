f64ad4fc208eabe48d3b115260c76b86775dae4c
RysteQ <34135554+RysteQ@users.noreply.github.com>
2025-12-25 21:21:40 +0200

    Finished the video output tab

--- src/EmuXUI/ViewModels/ExecutionViewModel.cs
+++ src/EmuXUI/ViewModels/ExecutionViewModel.cs
@@ -1,10 +1,52 @@
-﻿using EmuXUI.ViewModels.Internal;
+﻿using EmuXCore.Common.Interfaces;
+using EmuXCore.VM.Interfaces;
+using EmuXUI.Models.Observable;
+using EmuXUI.ViewModels.Internal;
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace EmuXUI.ViewModels;
 
 public sealed class ExecutionViewModel : BaseViewModel
 {
+    public ExecutionViewModel(IList<IInstruction> instructions, IVirtualMachine virtualMachine)
+    {
+        _instructions = instructions;
+        _virtualMachine = virtualMachine;
+
+        CommandExecuteCode = GenerateCommand(async () => await ExecuteCode());
+
+        VideoOutput = new(_virtualMachine.GPU.Height, _virtualMachine.GPU.Width);
+
+    }
+
+    private async Task ExecuteCode()
+    {
+        await VideoOutput.WriteEntireImage(_virtualMachine.GPU.Data);
+        VideoOutput.Update();
+    }
+
+    public async Task UpdateBitmap()
+    {
+        await VideoOutput.WriteEntireImage(_virtualMachine.GPU.Data);
+        VideoOutput.Update();
+    }
+
+    public Bitmap VideoOutput { get; private set; }
+    public ICommand CommandExecuteCode { get; private set; }
+
+    private readonly IList<IInstruction> _instructions;
+    private readonly IVirtualMachine _virtualMachine;
 }
--- src/EmuXUI/Views/ExecutionWindow.xaml.cs
+++ src/EmuXUI/Views/ExecutionWindow.xaml.cs
@@ -1,10 +1,15 @@
+using EmuXCore.Common.Interfaces;
 using EmuXCore.VM.Interfaces;
+using EmuXUI.ViewModels;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
@@ -24,12 +29,45 @@ namespace EmuXUI.Views;
 /// </summary>
 public sealed partial class ExecutionWindow : Window
 {
-    public ExecutionWindow(IVirtualMachine virtualMachine)
+    public ExecutionWindow(IList<IInstruction> instructions, IVirtualMachine virtualMachine)
     {
         InitializeComponent();
 
-        _virtualMachine = virtualMachine;
+        ViewModel = new(instructions, virtualMachine);
+        
+        InitUI();
     }
 
-    private IVirtualMachine _virtualMachine;
+    private async void Window_Activated(object sender, WindowActivatedEventArgs args)
+    {
+        await ViewModel.UpdateBitmap();
+    }
+
+    private void InitUI()
+    {
+        ExtendsContentIntoTitleBar = true;
+        SetTitleBar(TitleBarGrid);
+
+        AppWindow.Resize(new(ViewModel.VideoOutput.Width * 2 + 200, ViewModel.VideoOutput.Height * 2 + 100));
+
+        DisableWindowResizing();
+    }
+
+    private void DisableWindowResizing()
+    {
+        nint windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(this);
+        WindowId windowId = Win32Interop.GetWindowIdFromWindow(windowHandle);
+        AppWindow? applicationWindow = AppWindow.GetFromWindowId(windowId);
+        OverlappedPresenter? presenter = applicationWindow?.Presenter as OverlappedPresenter;
+
+        if (presenter == null)
+        {
+            return;
+        }
+
+        presenter.IsResizable = false;
+        presenter.IsMaximizable = false;
+    }
+
+    public ExecutionViewModel ViewModel { get; private set; }
 }
