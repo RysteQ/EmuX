4cba102397f016f915b514ab689f6bb9ecbed508
RysteQ <34135554+RysteQ@users.noreply.github.com>
2025-12-26 02:20:06 +0200

    Removed the video memory from the RAM module

--- src/EmuXCore/VM/Interfaces/Components/IVirtualMemory.cs
+++ src/EmuXCore/VM/Interfaces/Components/IVirtualMemory.cs
@@ -13,17 +13,17 @@ public interface IVirtualMemory : IVirtualComponent
     IDictionary<string, IMemoryLabel> LabelMemoryLocations { get; set; }
 
     /// <summary>
-    /// The RAM of the VMemory, must be the size of VIDEO_MEMORY + GENERAL_PURPOSE_MEMORY. It is not recommended to read / write directly to RAM, please use the Read / Write methods instead
+    /// The RAM of the VMemory, must be the size of IO_MEMORY + GENERAL_PURPOSE_MEMORY. It is not recommended to read / write directly to RAM, please use the Read / Write methods instead
     /// </summary>
     byte[] RAM { get; set; }
-
-    /// <summary>
-    /// The size allocated for the video memory
-    /// </summary>
-    uint VIDEO_MEMORY { get; }
 
     /// <summary>
     /// The size allocated for general purpose memory
     /// </summary>
     uint GENERAL_PURPOSE_MEMORY { get; }
+
+    /// <summary>
+    /// The size allocated for IO memory
+    /// </summary>
+    public uint IO_MEMORY { get; init; }
 }
--- src/EmuXCore/VM/Internal/Memory/VirtualMemory.cs
+++ src/EmuXCore/VM/Internal/Memory/VirtualMemory.cs
@@ -6,13 +6,12 @@ namespace EmuXCore.VM.Internal.Memory;
 
 public class VirtualMemory : IVirtualMemory
 {
-    public VirtualMemory(uint ioMemory, uint videoMemory, uint generalPurposeMemory, IVirtualMachine? parentVirtualMachine = null)
+    public VirtualMemory(uint ioMemory, uint generalPurposeMemory, IVirtualMachine? parentVirtualMachine = null)
     {
         IO_MEMORY = ioMemory;
-        VIDEO_MEMORY = videoMemory;
         GENERAL_PURPOSE_MEMORY = generalPurposeMemory;
 
-        RAM = new byte[IO_MEMORY + VIDEO_MEMORY + GENERAL_PURPOSE_MEMORY];
+        RAM = new byte[IO_MEMORY + GENERAL_PURPOSE_MEMORY];
         LabelMemoryLocations = new Dictionary<string, IMemoryLabel>();
 
         Random.Shared.NextBytes(RAM);
@@ -25,7 +24,6 @@ public class VirtualMemory : IVirtualMemory
     public IDictionary<string, IMemoryLabel> LabelMemoryLocations { get; set; }
 
     public uint IO_MEMORY { get; init; }
-    public uint VIDEO_MEMORY { get; init; }
     public uint GENERAL_PURPOSE_MEMORY { get; init; }
     public IVirtualMachine? ParentVirtualMachine { get; set; }
 }
