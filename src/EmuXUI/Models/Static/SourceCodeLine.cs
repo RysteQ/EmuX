339de037b3b60d2bdb0a691c03995c5121d2c765
RysteQ <34135554+RysteQ@users.noreply.github.com>
2025-12-26 05:04:27 +0200

    Added a new UI model for the source code line

---
+++ src/EmuXUI/Models/Static/SourceCodeLine.cs
@@ -0,0 +1,17 @@
+﻿using EmuXCore.Common.Interfaces;
+
+namespace EmuXUI.Models.Static;
+
+public sealed class SourceCodeLine
+{
+    public SourceCodeLine(int line, string sourceCode, IInstruction instruction)
+    {
+        Line = line;
+        SourceCode = sourceCode;
+        Instruction = instruction;
+    }
+
+    public int Line { get; set; }
+    public string SourceCode { get; init; }
+    public IInstruction Instruction { get; init; }
+}
