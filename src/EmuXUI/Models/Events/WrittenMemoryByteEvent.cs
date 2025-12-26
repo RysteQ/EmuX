42894c6e8dfcba9766b8c5d94e54abdf4dd57218
RysteQ <34135554+RysteQ@users.noreply.github.com>
2025-12-26 02:47:04 +0200

    Added events for memory writes and register writes

---
+++ src/EmuXUI/Models/Events/WrittenMemoryByteEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmuXUI.Models.Events;
+
+public sealed class WrittenMemoryByteEvent : EventArgs
+{
+    public WrittenMemoryByteEvent(int index, byte previousValue, byte newValue)
+    {
+        Index = index;
+        PreviousValue = previousValue;
+        NewValue = newValue;
+    }
+
+    public int Index { get; init; }
+    public byte PreviousValue { get; init; }
+    public byte NewValue { get; init; }
+}
---
+++ src/EmuXUI/Models/Events/WrittenToRegisterEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmuXUI.Models.Events;
+
+public sealed class WrittenToRegisterEvent : EventArgs
+{
+    public WrittenToRegisterEvent(string registerName, ulong previousValue, ulong newValue)
+    {
+        RegisterName = registerName;
+        PreviousValue = previousValue;
+        NewValue = newValue;
+    }
+
+    public string RegisterName { get; init; }
+    public ulong PreviousValue { get; init; }
+    public ulong NewValue { get; init; }
+}
