907ae9603507961edc011243a90d25cce5c889a5
RysteQ <34135554+RysteQ@users.noreply.github.com>
2025-12-26 02:47:40 +0200

    Added new models for the memory lookup and register lookup

---
+++ src/EmuXUI/Models/Observable/MemoryByte.cs
@@ -0,0 +1,28 @@
+﻿using EmuXUI.Models.Events;
+using EmuXUI.Models.Internal;
+using System;
+
+namespace EmuXUI.Models.Observable;
+
+public sealed class MemoryByte : ObservableObject
+{
+    public MemoryByte(int index, byte value)
+    {
+        Index = index;
+        Value = value;
+    }
+
+    public event EventHandler? ValueChanged;
+
+    public int Index { get; init; }
+    public byte Value
+    {
+        get => field;
+        set
+        {
+            ValueChanged?.Invoke(this, new WrittenMemoryByteEvent(Index, field, value));
+
+            OnPropertyChanged(ref field, value);
+        }
+    }
+}
---
+++ src/EmuXUI/Models/Observable/Register.cs
@@ -0,0 +1,47 @@
+﻿using EmuXCore.Common.Enums;
+using EmuXUI.Models.Events;
+using EmuXUI.Models.Internal;
+using System;
+
+namespace EmuXUI.Models.Observable;
+
+public sealed class Register : ObservableObject
+{
+    public Register(string name, Size size, ulong value)
+    {
+        Name = name;
+        Size = size;
+        Value = value;
+    }
+
+    public event EventHandler? ValueChanged;
+
+    public string Name { get; init; }
+    public Size Size { get; init; }
+    
+    public double Value
+    {
+        get => field;
+        set
+        {
+            ValueChanged?.Invoke(this, new WrittenToRegisterEvent(Name, (ulong)field, (ulong)value));
+
+            OnPropertyChanged(ref field, value);
+        }
+    }
+    
+    public double MaximumValue
+    {
+        get
+        {
+            return Size switch
+            {
+                Size.Byte => byte.MaxValue,
+                Size.Word => ushort.MaxValue,
+                Size.Dword => uint.MaxValue,
+                Size.Qword => ulong.MaxValue,
+                _ => 0
+            };
+        }
+    }
+}
