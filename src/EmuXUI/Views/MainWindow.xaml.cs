d3dfaa46d7a100527c4373349a13036c5b648b63
RysteQ <34135554+RysteQ@users.noreply.github.com>
2026-02-23 22:41:35 +0200

    Bumped version number

--- src/EmuXUI/Views/MainWindow.xaml.cs
+++ src/EmuXUI/Views/MainWindow.xaml.cs
@@ -107,7 +107,7 @@ public sealed partial class MainWindow : Window
         ContentDialog versionDialog = new()
         {
             Title = "About",
-            Content = "Created by RysteQ on GitHub\n\nVersion: 2.1.4",
+            Content = "Created by RysteQ on GitHub\n\nVersion: 2.2.4",
             CloseButtonText = "Okay"
         };
 
